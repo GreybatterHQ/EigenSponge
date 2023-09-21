49d7c1495746f5d0f8fadf4ee8dd17b6676ecc3e
AyushTejwani <tejwani.ayush@gmail.com>
2023-09-21 23:47:39 +0530

    amazonSearchWorking

--- amazonDataScraper.py
+++ amazonDataScraper.py
@@ -2,10 +2,27 @@ import json
 import requests
 import pandas as pd
 
-def amazon_search():
-    return null
-def amazon_product():
-    return null
+def amazon_search( api_basic_auth_token, query):
+    url = "https://scrape.smartproxy.com/v1/tasks"
+
+    payload = {
+      "target": "amazon_search",
+      "query": str(query),
+      "parse": True,
+      "domain": "in",
+      "device_type": "desktop",
+      "page_from": "1"
+      }
+
+    headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Basic "+ str(api_basic_auth_token)
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
 
 with open("config.json","r") as config_file:
     config = json.load(config_file)
@@ -16,7 +33,8 @@ api_basic_auth = config['api_basic_auth_token']
 
 excel= pd.read_excel('input.xlsx')
 
-print(excel[0])
+print(excel)
 
-products=amazon_search()
-amazon_product(products)
+for row in excel.itertuples():
+    print(row[1])
+    amazon_search(api_basic_auth_token=api_basic_auth,query=row[1])
